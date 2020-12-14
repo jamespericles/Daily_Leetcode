4b48e63d2fd05db927362bd7084667cd92fcefac
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-12-14 18:58:28 -0500

    I was bored and decided to do another question, peak index mountain array

---
+++ Week 3/mountainArray.js
@@ -0,0 +1,32 @@
+/* 
+    Prompt:
+
+    Let's call an array arr a mountain if the following properties hold:
+
+    arr.length >= 3
+    There exists some i with 0 < i < arr.length - 1 such that:
+        arr[0] < arr[1] < ... arr[i-1] < arr[i]
+        arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+    Given an integer array arr that is guaranteed to be a mountain, return any i such that arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+*/
+
+let arr = [0, 1, 0]; // should output 1
+
+let peakIndex = (arr) => {
+  // ensure arr actually contains numbers, if it doesn't, exit the function
+  if (arr.length === 0) {
+    return -1;
+  }
+
+  let max = arr[0];
+  let maxIndex = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      maxIndex = i;
+      max = arr[i];
+    }
+  }
+  return maxIndex;
+};
