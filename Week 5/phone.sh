c24c455be00fd1cfa335a069cd135c81bdd42470
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-12-29 14:40:14 -0500

    completed valid phone number

---
+++ Week 5/phone.sh
@@ -0,0 +1,14 @@
+#Given a text file file.txt that contains list of phone numbers (one per line), write a one liner bash script to print all valid phone numbers.
+
+#You may assume that a valid phone number must appear in one of the following two formats: (xxx) xxx-xxxx or xxx-xxx-xxxx. (x means a digit)
+
+#You may also assume each line in the text file must not contain leading or trailing white spaces.
+
+#RegEx in order: 
+            #any 3 numbers followed by a dash
+                  #any 3 numbers followed by a dash
+                        #any 4 numbers or...
+grep -P '^(\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4})$' file.txt
+                              #any 3 numbers wrapped with parens
+                                        #any 3 numbers not proceded by a dash
+                                                #any 4 numbers proceded by a dash  
