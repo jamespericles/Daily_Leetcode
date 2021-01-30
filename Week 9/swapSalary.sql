e992303bb5a38b5dc11c28e888245afc1a4ce38f
jamespericles <63256479+jamespericles@users.noreply.github.com>
2021-01-30 15:20:59 -0500

    completed swap-salary

---
+++ Week 9/swapSalary.sql
@@ -0,0 +1,11 @@
+-- Prompt: 
+--      Given a table salary, such as the one below, that has m=male and f=female values. Swap all f and m values (i.e., change all f values to m and vice versa) with a single update statement and no intermediate temp table.
+
+--      Note that you must write a single update statement, DO NOT write any select statement for this problem.
+-- https://leetcode.com/problems/swap-salary/submissions/
+UPDATE salary
+SET
+    sex = CASE sex
+        WHEN 'm' THEN 'f'
+        ELSE 'm'
+    END;
