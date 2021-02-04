1eae5a44c9b523463755374249a8eee81d2fa604
jamespericles <63256479+jamespericles@users.noreply.github.com>
2021-02-04 14:21:20 -0500

    completed nth highest salary

---
+++ Week 10/nthHighest.sql
@@ -0,0 +1,31 @@
+-- Write a SQL query to get the nth highest salary from the Employee table.
+
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+
+-- +------------------------+
+-- | getNthHighestSalary(2) |
+-- +------------------------+
+-- | 200                    |
+-- +------------------------+
+
+-- https://leetcode.com/problems/nth-highest-salary/
+
+CREATE FUNCTION getNthHighestSalary(N INT)
+RETURNS INT BEGIN
+SET N = N - 1;
+RETURN (
+# Write your MySQL query statement below.
+SELECT
+DISTINCT Salary
+FROM Employee
+ORDER BY Salary DESC
+LIMIT 1 OFFSET N
+);
+END
