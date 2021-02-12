0dd4182f35fb78fedee9cb3d270ef639e4297b4f
jamespericles <63256479+jamespericles@users.noreply.github.com>
2021-02-12 17:51:41 -0500

    completed valid palindrome in java

--- Week 10/Java/validPalindrome.java
+++ Week 10/Java/validPalindrome.java
@@ -13,5 +13,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
+        if (s.isEmpty())
+            return true;
+
+        int i = 0, j = s.length() - 1;
+
+        while (i <= j) {
+
+            if (!Character.isLetterOrDigit(s.charAt(i)))
+                i += 1;
+            else if (!Character.isLetterOrDigit(s.charAt(j)))
+                j -= 1;
+            else if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                return false;
+            else {
+                i += 1;
+                j -= 1;
+            }
+        }
+        return true;
     }
 }
+
+/**
+ * Here's a more sophisticated two-pointer solution. Runtime of 2ms (faster than
+ * 98.10%) and memory usage of 39.5mb (less than 41.84%)
+ */
