ca0b6896433c1fc234361613c62d91fb7e25fbcd
jamespericles <63256479+jamespericles@users.noreply.github.com>
2021-02-12 12:56:08 -0500

    uploaded a new javascript solution

--- Week 10/JavaScript/hammingWeight.js
+++ Week 10/JavaScript/hammingWeight.js
@@ -29,3 +29,23 @@ let hammingWeight = (n) => {
  * Similarly poor results, runtime of 164ms (faster than 5.55%) and memory usage of
  * 46.7mb (less than 6.24%)
  */
+
+let hammingWeight = (n) => {
+  let binary = n.toString(2),
+    count = 0;
+
+  let ones = binary.replace("0", "");
+
+  for (let i = 0; i < binary.length; i++) {
+    console.log(binary[i]);
+    if (binary[i] == 1) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+/**
+ * Refactored the Java solution to see if that would improve JavaScript. Ironically, I
+ * received the exact same results as before.
+ */
