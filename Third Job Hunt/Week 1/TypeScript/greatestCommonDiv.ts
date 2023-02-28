c28be18aa00dd9ec06360641c375c0cdf4974f6c
James Pericles <jamesspericles@gmail.com>
2023-02-28 14:37:37 -0600

    finished greatest common divisor

---
+++ Third Job Hunt/Week 1/TypeScript/greatestCommonDiv.ts
@@ -0,0 +1,22 @@
+/**
+ * Prompt:
+ *
+ * Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+ *
+ * The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+ */
+
+const findGCD = (nums: number[]): number => {
+  // First determine the smallest and largest numbers in the array
+  const min = Math.min(...nums)
+  const max = Math.max(...nums)
+
+  // Then determine the greatest common divisor of the two numbers by using a modulo operator on both numbers
+  for (let i = min; i > 0; i--) {
+    if (min % i === 0 && max % i === 0) {
+      return i
+    }
+  }
+
+  return 1
+}
